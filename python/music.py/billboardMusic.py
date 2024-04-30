284678396036e15be736a8ff3bfd21c6e6c184b8
머원 <sdw10195@gmail.com>
2024-04-30 17:59:13 +0900

    3

--- python/music.py/billboardMusic.py
+++ python/music.py/billboardMusic.py
@@ -1,33 +1,47 @@
-import requests as req
-from bs4 import BeautifulSoup as bs
-import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+import json
+import datetime
 
-head = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
-res = req.get("https://www.billboard.com/charts/hot-100/", headers=head)
-soup = bs(res.text, "lxml")
+# 현재 날짜를 문자열로 저장
+current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-ranking = soup.select(".pmc-paywall .o-chart-results-list-row-container .o-chart-results-list__item >span")
-title = soup.select(".pmc-paywall .lrv-u-width-100p .o-chart-results-list__item> h3")
-artist = soup.select(".pmc-paywall .lrv-u-width-100p .o-chart-results-list__item> span")
+# 웹 페이지로부터 데이터 요청 및 수신
+res = requests.get("https://www.billboard.com/charts/hot-100/")
+soup = BeautifulSoup(res.text, "lxml")
 
+# print(res.text)
+# print(res.status_code) 
 
-rankingList = []
-titleList = []
-artistList = []
+# 데이터 선택
+ranking = soup.select(".o-chart-results-list-row-container > ul > li.o-chart-results-list__item:nth-child(1) > span.c-label:nth-child(1)")
+title = soup.select(".o-chart-results-list-row-container > ul > li.lrv-u-width-100p > ul > li:nth-child(1) > h3.c-title")
+artist = soup.select(".o-chart-results-list-row-container > ul > li.lrv-u-width-100p > ul > li:nth-child(1) > span.c-label")
+image = soup.select(".o-chart-results-list-row-container ul > li:nth-child(2) .c-lazy-image > div > img")
 
-print(len(ranking))
-print(len(artist))
-print(len(title))
+# print(len(image))
 
-rankingList = [r.text.strip() for r in ranking]
-titleList = [t.text.strip() for t in title]
-artistList = [a.text.strip() for a in artist]
+# 데이터 저장
+rankings = [r.text.strip() for r in ranking]
+titles = [t.text.strip() for t in title]
+artists = [a.text.strip() for a in artist]
+images = [img.get('data-src') or img.get('data-lazy-src') or img.get('src') for img in image]
 
-chart_df = pd.DataFrame({
-    'Ranking': rankingList,
-    'Title': titleList,
-    'Artist': artistList
-})
+# print(images)
 
-# JSON 파일로 저장 
-# chart_df.to_json("billboard.json", force_ascii=False, orient="records")
+# 데이터 프레임 생성
+chart_data = []
+for ranking, title, artist, image_url in zip(rankings, titles, artists, images):
+    chart_data.append({
+        "rank": ranking,
+        "title": title,
+        "artist": artist,
+        "imageURL": image_url
+    })
+
+# 파일 이름 설정
+file_name = f"billboard100_{current_date}.json"
+
+# JSON 파일로 저장
+with open(file_name, 'w', encoding='utf-8') as file:
+    json.dump(chart_data, file, ensure_ascii=False, indent=4)
